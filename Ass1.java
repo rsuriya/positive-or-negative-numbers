6bb725fec04ec9d209cc9f8a788e0e2e7d4ebaa6
rsuriya <rsuriya0396@gmail.com>
2016-07-20 11:25:39 +0530

    k
    gh

---
+++ Ass1.java
@@ -0,0 +1,21 @@
+
+public class Ass1 {
+
+	
+	public static void main(String[] args) {
+		int a;
+		a=5;
+		if(a>0)
+		{
+			System.out.println("number is positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("number is negative");
+		}
+		else
+			{
+			System.out.println("number is neither nor positive");}
+			}
+
+}
